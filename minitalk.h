78076770ba2a6fffac2801a3607dbe591cb7eee7
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-07 12:39:55 -0300

    add header for includes and global var

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 12:37:29 by gschiavo          #+#    #+#             */
+/*   Updated: 2022/10/07 12:38:23 by gschiavo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+#endif

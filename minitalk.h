ec6b95bb5ea57715bae4e0b84055921318b297a2
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2022-10-07 13:26:17 -0300

    Add global variable

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: gschiavo <gschiavo@42student.org.br>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 12:37:29 by gschiavo          #+#    #+#             */
-/*   Updated: 2022/10/07 12:38:23 by gschiavo         ###   ########.fr       */
+/*   Updated: 2022/10/07 13:04:43 by gschiavo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/libft.h"
+
+static pid_t			*server_pid = 0;
 
 #endif
